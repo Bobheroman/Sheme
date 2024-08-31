e110ebe066e871bca623766c25478531e31e2f0a
bobhe <bobhe@DESKTOP-R28NMSL>
2024-08-31 18:11:02 +0300

    Добавьте файлы проекта.

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const componentKD2 = document.getElementById('component-kd2');
+    const componentKD1 = document.getElementById('component-kd1');
+    const wire1 = document.getElementById('wire-1');
+    const wire2 = document.getElementById('wire-2');
+
+    componentKD2.addEventListener('click', () => {
+        // Подсвечиваем компоненты и провода
+        componentKD2.classList.toggle('highlight');
+        componentKD1.classList.toggle('highlight');
+        wire1.classList.toggle('highlight');
+        wire2.classList.toggle('highlight');
+    });
+
+});
